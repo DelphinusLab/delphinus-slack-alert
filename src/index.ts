fd42d71753e119ca5cf2fa7f07a385def2bbcb7a
yizong <ziper1540101@gmail.com>
2022-08-15 16:30:19 +1000

    CROS-50 refine

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { textCmpPreprosessor } from './tools/tool';
+import { textCmpPreprosessor } from './lib';
 
 export type slackConfig = {
     active: boolean,
--- src/test.ts
+++
@@ -1,9 +0,0 @@
-import { sendAlert } from './index'
-const TestConfig = require("./test-config");
-
-function main () {
-    const err = new Error('Test Error');
-    sendAlert(err, TestConfig);
-}
-
-main();
