684806113685502dd6192db8cae95d7449252933
yymone <yymone84@hotmail.com>
2022-09-29 20:21:49 +1000

    CROS-67 fix async function await

--- src/sendToSlackTool/sendToSlack.ts
+++ src/sendToSlackTool/sendToSlack.ts
@@ -1,9 +1,9 @@
 import { sendAlert } from '../index'
 const config = require("./config");
 
-function main () {
+async function main () {
     const err = new Error('Test Error');
-    sendAlert(err, config, false);
+    await sendAlert(err, config, false);
 }
 
 main();
