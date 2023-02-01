f45e46a907c4593d0825a7583aeb6ec6323edeba
RuilaiZhang <ray.zhangruilai@gmail.com>
2023-02-01 16:15:46 +1100

    test

--- src/sendToSlackTool/sendToSlack.ts
+++ src/sendToSlackTool/sendToSlack.ts
@@ -1,12 +1,5 @@
 import { sendAlert } from '../index'
-require('dotenv').config();
-
-const config = {
-    "active": true,
-    "channelId": process.env["CHANNELID"],
-    "token": process.env["TOKEN"],
-    "timePeriod": 300
-};
+const config = require("./config");
 
 async function main () {
     const err = new Error('Test Error');
