e551616a05cce943080737a8d1079d060b069344
rhaoio <richard@rhao.io>
2024-10-14 16:31:21 +1100

    use slack package

--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,79 @@
-import axios from 'axios';
-import { textCmpPreprosessor } from './lib';
+import { textCmpPreprosessor } from "./lib";
 
-export type slackConfig = {
-    active: boolean,
-    channelId?: string,
-    token?: string,
-    timePeriod?: number
+import { WebClient } from "@slack/web-api";
+
+export type SlackConfig = {
+  channelId?: string;
+  token?: string;
+  timePeriod?: number;
 };
 
-async function requestSlackHelper(method: string, url: string, params: any, data: any, token: string) {
-    const headers = {
-        'Content-Type': 'application/json; charset=utf-8',
-        'Authorization': 'Bearer ' + token,
-    };
-    try {
-        const res = await axios({
-            method,
-            url,
-            params,
-            headers,
-            data
-        });
-        if (!res.data.ok) {
-            throw new Error('Failed to alert in Slack: ' + res.data.error);
-        };
-        return res.data;
-    } catch(e) {
-        console.log(e);
-    }
-}
-
-async function fetchHistoryMessages(token: string, channelId: string, timePeriod: number) {
-    const url = "https://slack.com/api/conversations.history";
-    const params = {
-        'channel': channelId
-    };
-    const historyMsg = (await requestSlackHelper('GET', url, params, {}, token)).messages;
-    const localTimeStamp = Math.floor(new Date().getTime() / 1000);
-    const historyMsgInRange = historyMsg.filter((msg: { "ts": string }) => localTimeStamp - Number(msg.ts) < timePeriod);
-    return historyMsgInRange.map((msg: {"text": string}) => msg.text.replace(/\s/gm, ''));
+export function createNewSlackClient(token: string) {
+  return new WebClient(token);
 }
 
 async function sendNewMessage(token: string, text: string, channelId: string) {
-    const url = "https://slack.com/api/chat.postMessage";
-    const data = {
-        channel: channelId,
-        text: text
-    };
-    await requestSlackHelper('POST', url, {}, data, token);
+  const args = {
+    channel: channelId,
+    text: text,
+  };
+  await createNewSlackClient(token).chat.postMessage(args);
 }
 
-export async function sendAlert(text: any, config: slackConfig, printOut: boolean = true) {
-    const newMessage = text.toString() + '.\n' + text.stack ? text.stack : '';
-    if(printOut) {
-        console.log(newMessage);
+async function fetchPreviousMessages(
+  token: string,
+  channelId: string,
+  timePeriod: number = 0
+) {
+  const client = createNewSlackClient(token);
+  const result = await client.conversations.history({
+    channel: channelId,
+  });
+
+  const messages = result.messages;
+
+  if (!messages) {
+    return [];
+  }
+  const currentTimestamp = Date.now() / 1000;
+  const historyMessages = messages.filter((message) => {
+    if (!message.ts) {
+      return false;
     }
-    if(!config.active) {
-        return;
-    } 
-    if(!config.channelId || !config.token || !config.timePeriod) {
-        console.log('Failed to alert in Slack: Invalid values in config file\n');
-        return;
+    // Only check the timePeriod if it is set > 0, otherwise just return all values.
+    if (timePeriod <= 0) {
+      return true;
     }
-    const historyMessages = await fetchHistoryMessages(config.token, config.channelId, config.timePeriod);
-    if(!historyMessages.includes(textCmpPreprosessor(newMessage))) {
-        await sendNewMessage(config.token, newMessage, config.channelId);
-    };
+    // Parse the timestamp from the message
+    const messageTimestamp = parseFloat(message.ts);
+
+    // Check if the message is older than the time period we are interested in
+    return currentTimestamp - messageTimestamp < timePeriod;
+  });
+  return historyMessages;
+}
+
+export async function sendAlert(
+  text: string,
+  config: SlackConfig,
+  printOut: boolean = true
+) {
+  if (printOut) {
+    console.log(text);
+  }
+
+  if (!config.channelId || !config.token || !config.timePeriod) {
+    console.log("Failed to alert in Slack: Invalid values in config file\n");
+    return;
+  }
+  const prevMessages = await fetchPreviousMessages(
+    config.token,
+    config.channelId,
+    config.timePeriod
+  );
+  const prevMessagesText = prevMessages.map((message) => message.text);
+  // Check if the message has already been sent within the specified time period
+  if (!prevMessagesText.includes(textCmpPreprosessor(text))) {
+    await sendNewMessage(config.token, text, config.channelId);
+  }
 }
--- src/sendToSlackTool/sendToSlack.ts
+++ src/sendToSlackTool/sendToSlack.ts
@@ -1,9 +1,10 @@
-import { sendAlert } from '../index'
+import { sendAlert } from "../index";
 const config = require("./slack-alert-config");
 
-async function main () {
-    const err = new Error('Test Error');
-    await sendAlert(err, config, false);
+async function main() {
+  const err = new Error("Test Error");
+  const errorText = `Error: ${err.message}`;
+  await sendAlert(errorText, config, false);
 }
 
 main();
